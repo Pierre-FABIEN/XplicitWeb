ad2c175804943b22cdf8ba104aa43b457c6ab582
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-12-24 13:52:42 +0100

    catalogue wip

--- src/routes/catalogue/+page.svelte
+++ src/routes/catalogue/+page.svelte
@@ -1,5 +1,32 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-
-	let { data }: { data: PageData } = $props();
+	import * as Card from '$shadcn/card/index.js';
+	import * as Carousel from '$shadcn/carousel/index.js';
 </script>
+
+<div class="h-[100vh] w-[50vw] ccc">
+	<div class="h-[80vh] w-[50vw] ccc">
+		<Carousel.Root
+			opts={{
+				align: 'start'
+			}}
+			orientation="vertical"
+			class="w-[100%] ccc"
+		>
+			<Carousel.Content class="-mt-1 h-[60vh]">
+				{#each Array(5) as _, i (i)}
+					<Carousel.Item class="pt-1 md:basis-1/2">
+						<div class="p-1 w-[400px] h-[400px]">
+							<Card.Root>
+								<Card.Content class="w-[400px] h-[350px] flex items-center justify-center p-6">
+									<span class="text-3xl font-semibold">{i + 1}</span>
+								</Card.Content>
+							</Card.Root>
+						</div>
+					</Carousel.Item>
+				{/each}
+			</Carousel.Content>
+			<Carousel.Previous />
+			<Carousel.Next />
+		</Carousel.Root>
+	</div>
+</div>
